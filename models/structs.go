4bb75d6a8df1462621da8ac3589e8596b5e6fcc8
kwabsntim <daffourkwabena@gmail.com>
2025-11-08 16:47:19 +0000

    some changes

--- models/structs.go
+++ models/structs.go
@@ -8,7 +8,7 @@ type User struct {
 	ID        string    `bson:"_id,omitempty" json:"id"`
 	Username  string    `bson:"usernme" json:"username"`
 	Email     string    `bson:"email" json:"email"`
-	Password  string    `bson:"password" json:"-"`
+	Password  string    `bson:"password" json:"password"`
 	CreatedAt time.Time `bson:"createdAt" json:"created_at"`
 	LastLogin time.Time `bson:"lastLogin" json:"last_login"`
 	UpdatedAt time.Time `bson:"updated" json:"updated_at"`
